b5ab7766b307f2a874b2e8ce44e746e88370f86c
VoJohnson <jvo001@student.butte.edu>
2025-09-23 18:15:26 +0000

    step 2

---
+++ activity_4_1/sets.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <array>
+
+int main() {
+    std::array<int, 10> Array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    std::printf(6i=10);
+
+    std::array<char, 5> Alpha = {'a', 'b', 'c', 'd', 'e'};
+
+    std::printf(10i=5)
+
+    return 0;
+}
