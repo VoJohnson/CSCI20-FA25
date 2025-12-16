d2464912ce158b228bcad457f34c3e42dcc286f4
VoJohnson <jvo001@student.butte.edu>
2025-12-16 21:27:50 +0000

    almost

--- OBJ Calc/Obj Calc.cpp
+++ OBJ Calc/Obj Calc.cpp
@@ -1,10 +1,37 @@
-#include "Obj Header.hpp" 
+#include "Obj Header.hpp"
 
 int main() {
+    Calc calc;
+    vector<Calc::stuff> load_eq = {};
+
+    //test: 1 + 1
+    Calc::stuff x(0, '+');
+    Calc::stuff y(1, 'e');
+    Calc::stuff z(1, 'e');
+
+    load_eq.push_back(x);
+    load_eq.push_back(y);
+    load_eq.push_back(z);
+
+    calc.map(load_eq);
 
     return 0;
 }
 
-void Calc::road_in() {
+void Calc::machine(vector<stuff> x) {
+    map(x);
+    return ;
+}
 
+vector<Calc::thing> Calc::map(vector<Calc::stuff> x) {
+    vector<Calc::thing> ret= {};
+    for(int i=0;i<x.size();i=i+1) {
+        // the set in the case of x.size = 3 i s[i-0,i=1,i=2, TERMINATE]
+        //for each stuff, make a new thing
+        thing c(x.at(i).trash, x.at(i).plastic);
+        ret.push_back(c);
+        printf("%d, %c", c.num, c.op);
+    };
+
+    return ret;
 }
--- OBJ Calc/Obj Header.hpp
+++ OBJ Calc/Obj Header.hpp
@@ -1,26 +1,29 @@
 #include <cstdio>
 #include <stack>
-#include <string>
+#include <vector>
 
+using std::vector;
 using std::stack;
 using std::printf;
 
 /*
 stack calculator built using a monolithic object.
 
-this means that the object will obfuscate the structure (stack) of evaulation.
+this means that the object will obfuscate the structure (stack) of evaulation. [solved]
+    - stack in private of class
 
 this object represents something like a simple calculator taking numbers and evaulating
 
 - problem: representing numbers and operations [solved]
-    we need an encoding for the numbers and operations of our calculator, lets use a struc and operations of our calculator, lets use a struct.
+    we need an encoding for the numbers and operations of our calculator,
+        lets use a struc and operations of our calculator, lets use a struct.
     our structure houses numbers for eval and characters for operations
     the operations will represented by the following:
     - '+' addition
     - '*' subtraction
     - '^' multiplication
     - '5' division
-    - 'e' evaluation
+    - 'e' evaluation (fail safe for evaluatable number detection)
 
 - problem: what are the basic operations of this calculator [SOLVED]
     binary operators on which two numbers will be given (a,b)
@@ -35,9 +38,10 @@ this object represents something like a simple calculator taking numbers and eva
     - stack of type dicho b/c dicho assist in evaulation operation encoding.
 
 - problem: we need to be able to load this stack (st) in proper order. [UNSOLVED]
- -get data from user
-    - How: Constructor(limits object to single use) or function
-    - Where: 
+    - get data from user
+        - How: 
+        - Where: Constructor(limits object to single use) or function
+            - take formatted user data map
 
 - problem: we need a way to evaulate a full stack. [UNSOLVED]
 
@@ -45,21 +49,32 @@ this object represents something like a simple calculator taking numbers and eva
 
 class Calc {
 private:
-    struct dicho {
+    struct thing {
         int num;
         char op;
+        thing(int a, char b) {
+            num = a;
+            op = b;
+        }
     };
-    stack<dicho> st;
+    stack<thing> gold;
     int addition(int, int);
     int subtraction(int, int);
     int multiplication(int, int);
     int division(int, int);
 public:
-    Calc(); //initializes values
-    void road_in(vector<box>);
-    struct box {
-        int num;
-        char op;
+    struct stuff {
+        int trash;
+        char plastic;
+        stuff(int a, char b) {
+            trash = a;
+            plastic = b;
+        }
     };
-    
+    // tool function
+    vector<thing> map(vector<stuff>);
+    Calc(); // initializes values
+
+    // loads the stack
+    void machine(vector<stuff>); // loads stack with values
 };
