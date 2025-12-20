2e33d217d63de81720a0300d628b4889f8ef0358
VoJohnson <jvo001@student.butte.edu>
2025-12-20 17:14:35 +0000

    DONE

--- OBJ Calc/Obj Calc.cpp
+++ OBJ Calc/Obj Calc.cpp
@@ -1,9 +1,44 @@
 #include "Obj Header.hpp"
 
+int main() {
+    Calc calc;
+    vector<Calc::stuff> load_eq = {};
+
+    // test: 1 + 1
+    Calc::stuff x(0, '+');
+    Calc::stuff y(1, 'e');
+    Calc::stuff z(1, 'e');
+
+    load_eq.push_back(x);
+    load_eq.push_back(y);
+    load_eq.push_back(z);
+
+    calc.machine(load_eq);
+
+    return 0;
+
+}void Calc::machine(vector<stuff> x) {
+    vector<Calc::thing> map_result = map(x);
+    for (int i=x.size();i>=0;i=i-1) {
+        waste.push(map_result.at(i));
+    }
+    return ;
+}
+
+vector<Calc::thing> Calc::map(vector<Calc::stuff> x) {
+    vector<Calc::thing> ret= {};
+    for(int i=0;i<x.size();i=i+1) {
+        // the set in the case of x.size = 3 i s[i-0,i=1,i=2, TERMINATE]
+        // for each stuff, make a new thing
+        thing c(x.at(i).trash, x.at(i).plastic);
+        ret.push_back(c);
+        printf("%d, %c\n", c.num, c.op);
+    }
+    return ret;
+
+}
 stack<int> Calc::evaluate (stack<int>) st {
     
-int main() {
-
     int x = st.top();
     st.pop();
 
@@ -44,57 +79,5 @@ int main() {
     } else {
         printf("FAIL\n");
     }
-
+}
     return st;
-}
-    Calc calc;
-    vector<Calc::stuff> load_eq = {};
-
-    //test: 1 + 1
-    Calc::stuff x(0, '+');
-    Calc::stuff y(1, 'e');
-    Calc::stuff z(1, 'e');
-
-    load_eq.push_back(x);
-    load_eq.push_back(y);
-    load_eq.push_back(z);
-
-    calc.machine(load_eq);
-
-    // need a stack b/c stack BASED calculator
-    stack<int> st  = {};
-
-    // load the stack CAREFULLY
-    st.push(9);
-    st.push(9998);
-    st.push(5);
-    st.push(5);
-    st.push(9999);
-
-    // run evaluate to evaluate the stack as an equation
-    st = calc.evaluate(st);
-
-    // printing the result
-    printf("result: %d\n", st.top());
-    return 0;
-}
-
-void Calc::machine(vector<stuff> x) {
-    vector<Calc::thing> map_result = map(x);
-    for (int i=x.size();i>=0;i=i-1) {
-        waste.push(map_result.at(i));
-    }
-    return ;
-}
-
-vector<Calc::thing> Calc::map(vector<Calc::stuff> x) {
-    vector<Calc::thing> ret= {};
-    for(int i=0;i<x.size();i=i+1) {
-        // the set in the case of x.size = 3 i s[i-0,i=1,i=2, TERMINATE]
-        // for each stuff, make a new thing
-        thing c(x.at(i).trash, x.at(i).plastic);
-        ret.push_back(c);
-        printf("%d, %c\n", c.num, c.op);
-    }
-    return ret;
-}
